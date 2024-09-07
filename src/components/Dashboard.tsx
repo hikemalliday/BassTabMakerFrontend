87d24b9c4c09f8f1074231ba3bce5ccc7ecca86a
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2024-09-07 17:44:17 -0500

    git hub action test

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -9,6 +9,7 @@ export const Dashboard = () => {
   const [songNamesModalOpen, setSongNamesModalOpen] = useState(false);
   const { songNames } = useSongContext();
 
+  console.log("test");
   return (
     <div className="dashboard-container">
       <div className="dashboard-title">Bass Tab Maker</div>
